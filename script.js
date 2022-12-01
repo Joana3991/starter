9e3579eba400a2912b5f93571fd6b65c1a8188ec
Joana Areas <jo_areas@hotmail.com>
2022-12-01 18:38:49 -0300

    open modal function

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnClose = document.querySelector('.close-modal');
 const btnsShow = document.querySelectorAll('.show-modal');
+
+const openModal = function() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+for (let i = 0; i < btnsShow.length; i++){
+  btnsShow[i].addEventListener('click', openModal);
+}
