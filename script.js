b4c1e705c9507f10e2adc2b0fea0b30e13c13b71
Joana Areas <jo_areas@hotmail.com>
2022-12-01 18:26:14 -0300

    saved all selectors

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnClose = document.querySelector('.close-modal');
+const btnsShow = document.querySelectorAll('.show-modal');
